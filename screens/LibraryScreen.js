600dcd3dac335067cb4cf5f544c225c3856a4bec
mrtanatorn44 <mrtanatorn44@gmail.com>
2022-10-30 06:06:13 +0700

    Create library screen

--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -1,17 +1,250 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View ,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View , Image, ScrollView, FlatList, TouchableOpacity} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native';
+import { Octicons, MaterialIcons } from '@expo/vector-icons'; 
+
+const userLibraryData = [
+  {
+    name: 'Playlist #1',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #2',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #3',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #4',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #5',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #6',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #7',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #8',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #9',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #10',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #11',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+  {
+    name: 'Playlist #12',
+    image: [
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+      'https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws=',
+      'https://media.istockphoto.com/photos/canadian-rockies-banff-national-park-dramatic-landscape-picture-id1342152935?b=1&k=20&m=1342152935&s=170667a&w=0&h=q9-vhO5MC7zwaxX8_zFUiqMnQJ5udMjEBf0npeCCAGs=',
+      'https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000'
+    ]
+  },
+]
+const userLibraryRowData = userLibraryData.reduce(function (rows, key, index) { 
+  return (index % 2 == 0 ? rows.push([key]) 
+    : rows[rows.length-1].push(key)) && rows;
+}, []);
 
 export default function LibraryScreen() {
   const navigation = useNavigation();
+  const page = 'library'
+  const [myText, setMyText] = useState('');
+
+  const renderBrowse = ({ item }) => (
+    <View>
+      <TouchableOpacity 
+        style={styles.libraryItem} 
+        onPress={() => setMyText(item[0].name)}
+      >
+        <View style={{flexDirection: 'row',width: '90%', height: '70%'}}>
+          <View style={{width: '50%', height: '100%'}}>
+            <Image
+              style={{aspectRatio: 1, width: '100%', borderTopLeftRadius: 10}}
+              source={{ uri: item[0].image[0]}}
+            />
+            <Image
+              style={{aspectRatio: 1, width: '100%', borderBottomLeftRadius: 10}}
+              source={{ uri: item[0].image[1]}}
+            />
+          </View>
+          <View style={{width: '50%', height: '100%'}}>
+            <Image
+              style={{aspectRatio: 1, width: '100%', borderTopRightRadius: 10}}
+              source={{ uri: item[0].image[2]}}
+            />
+            <Image
+              style={{aspectRatio: 1, width: '100%', borderBottomRightRadius: 10}}
+              source={{ uri: item[0].image[3]}}
+            />
+          </View>
+        </View>
+        <View style={{width: '90%', height: '20%'}}>
+          <Text style={styles.text} numberOfLines={1} >{item[0].name}</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity 
+        style={styles.libraryItem} 
+        onPress={() => setMyText(item[1].name)}
+      >
+        <View style={{flexDirection: 'row',width: '90%', height: '70%'}}>
+          <View style={{width: '50%', height: '100%'}}>
+            <Image
+              style={{aspectRatio: 1, width: '100%', borderTopLeftRadius: 10}}
+              source={{ uri: item[0].image[0]}}
+            />
+            <Image
+              style={{aspectRatio: 1, width: '100%', borderBottomLeftRadius: 10}}
+              source={{ uri: item[0].image[1]}}
+            />
+          </View>
+          <View style={{width: '50%', height: '100%'}}>
+            <Image
+              style={{aspectRatio: 1, width: '100%', borderTopRightRadius: 10}}
+              source={{ uri: item[0].image[2]}}
+            />
+            <Image
+              style={{aspectRatio: 1, width: '100%', borderBottomRightRadius: 10}}
+              source={{ uri: item[0].image[3]}}
+            />
+          </View>
+        </View>
+        <View style={{width: '90%', height: '20%'}}>
+          <Text style={styles.text} numberOfLines={1} >{item[1].name}</Text>
+        </View>
+      </TouchableOpacity> 
+    </View>
+  );
 
   return (
     <SafeAreaView style={styles.container}>
       {/* CONTENT */}
       <View style={styles.content}>
-        <Text>Content</Text>
+        {/* Page */}
+        <View style={{marginVertical: '3%', flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text style={styles.textBold}>Your Library</Text>
+          <TouchableOpacity style={{width: '15%', justifyContent: 'center'}}>
+            <Octicons style={{textAlign: 'center'}} name="gear" size={32} color="white" />
+          </TouchableOpacity>
+        </View>
+
+        {/* Browse */}
+        <View style={styles.library}>
+          <FlatList
+            horizontal={true} 
+            style={styles.libraryScroll}
+            data={userLibraryRowData}
+            renderItem={renderBrowse}
+            // keyExtractor={(playlist) => playlist.id}
+          />
+        </View>
+        
+        {/* User Activities */}
+        <View style={styles.library}>
+          <View style={{flexDirection: 'row', paddingBottom: 10}}>
+            <Text style={styles.text}>Your Activities</Text>
+          </View>
+          <TouchableOpacity style={{flexDirection: 'row', paddingVertical: 10}}>
+            <Octicons style={{width: '15%'}} name="heart-fill" size={32} color="white" />
+            <Text style={[styles.text, {width: '70%'}]}>Liked Songs</Text>
+            <MaterialIcons style={{width: '15%', textAlign: 'center'}} name="keyboard-arrow-right" size={32} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity style={{flexDirection: 'row', paddingVertical: 10}}>
+            <Octicons style={{width: '15%'}} name="people" size={32} color="white" />
+            <Text style={[styles.text, {width: '70%'}]}>Followed Artists</Text>
+            <MaterialIcons style={{width: '15%', textAlign: 'center'}} name="keyboard-arrow-right" size={32} color="white" />
+          </TouchableOpacity>
+        </View>
+
       </View>
+
+      {/* Footer */}
+      <View style={styles.footer}>
+        <TouchableOpacity style={{flex: 1, alignItems: 'center'}}>
+          <Octicons name="home" size={32} color={page=='home' ? "#1ED760" : "#fff"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{flex: 1, alignItems: 'center'}}>
+          <Octicons name="search" size={32} color={page=='discover' ? "#1ED760" : "#fff"} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{flex: 1, alignItems: 'center'}}>
+          <Octicons name="stack" size={32} color={page=='library' ? "#1ED760" : "#fff"} />
+        </TouchableOpacity>
+      </View>
+
     </SafeAreaView>
   );
 }
@@ -19,13 +252,52 @@ export default function LibraryScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    backgroundColor: 'white',
+    backgroundColor: '#1ED760'
   },
   content: {
-    flex: 10,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'lightgray',
+    flex: 1,
+    backgroundColor: 'black',
+    paddingHorizontal: '5%',
+    paddingVertical: '10%'
   },
+  footer: {
+    position: 'absolute',
+    width: '100%',
+    height: '10%',
+    bottom: 0,
+    backgroundColor: '#0E0E0F',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+
+  textBold: {
+    color: 'white',
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+  },
+
+  library: {
+    marginVertical: '3%'
+  },
+  libraryScroll: {
+    // backgroundColor: 'green',
+    height: '60%',
+    width: '100%'
+  },
+  libraryItem: {
+    flexDirection: 'column',
+    backgroundColor: '#0E0E0F',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    borderRadius: 20,
+    width: 200,
+    height: '48%',
+    margin: 5,
+  },
+
 });
