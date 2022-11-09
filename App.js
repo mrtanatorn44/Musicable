f9a178aa95fd4b1de995b3b4e7840ada892295ea
mrtanatorn44 <mrtanatorn44@gmail.com>
2022-11-09 20:04:16 +0700

    Install Toast and Slider

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Navigator } from './navigations/Navigator';
+import { RootSiblingParent } from 'react-native-root-siblings';
+
 // import { Provider } from 'react-redux'
 // import { store } from './stores/store'
 
 export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
+      <RootSiblingParent>
       {/* <Provider store={store}> */}
         <Navigator/>
       {/* </Provider> */}
+      </RootSiblingParent>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
